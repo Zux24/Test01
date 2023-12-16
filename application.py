7b82ae2295aef4b480d5e5d608244acc98a8f250
Laura Rozas <laurarozasg@gmail.com>
2023-12-15 22:54:43 -0300

    update app

--- application.py
+++ application.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def index():
-    return 'Hello World!'
+    # Hacer una solicitud HTTP para obtener los datos del seguro
+    response = requests.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/58')
+    
+    # Verificar si la solicitud fue exitosa (código de estado 200)
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Acceder a los datos del seguro
+        name = data['insurance']['name']
+        description = data['insurance']['description']
+        price = data['insurance']['price']
+        image = data['insurance']['image']
+        
+        # Renderizar la plantilla HTML con los datos
+        return render_template('index.html', name=name, description=description, price=price, image=image)
+    else:
+        # Manejar el caso de error
+        return "Error al obtener los datos del seguro"
 
 if __name__ == '__main__':
-    application.run(host="0.0.0.0", port=80, debug = True)
+    app.run(debug=True)
